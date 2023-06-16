c8448121a9af01a9dc303ce9a43ab8ea607fe9e8
SAIMAALI\LENOVO <saima.miti7@gmail.com>
2023-06-16 17:34:07 +0600

    initial commit

---
+++ C#AccessModifierr/C#AccessModifierr/BasicAccessModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_AccessModifierr
+{
+    internal class BasicAccessModifier
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Bicycle bicycle = new Bicycle();
+            bicycle.Wheels = 3;
+            bicycle.AttachedWheel();
+            bicycle.StartBicycle();
+            bicycle.GenerateInvoice();
+        }
+    }
+}
+
---
+++ C#AccessModifierr/C#AccessModifierr/NewAccessModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_AccessModifierr
+{
+	public class SellBicycle
+    {
+        private int Price { get; set; }
+        protected int Amount { get; set; }
+        protected void SetPrice(int price)
+        {
+            price = price;
+        }
+        protected void Invoice()
+        {
+            Console.WriteLine($"Your total is{Price * Amount}");
+        }
+    }
+
+}
